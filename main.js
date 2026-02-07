76a7d62d1133283ce3a980a409eb6a303ca0e8e2
Patrickleondev <gkpl0010@gmail.com>
2026-02-07 15:32:59 +0000

    feat: final polish - sound effects, title update, blog fix

--- main.js
+++ main.js
@@ -87,18 +87,18 @@ function playHoverSound() {
     const oscillator = audioCtx.createOscillator();
     const gainNode = audioCtx.createGain();
 
-    oscillator.type = 'sine';
-    oscillator.frequency.setValueAtTime(220, audioCtx.currentTime); // Low frequency
-    oscillator.frequency.exponentialRampToValueAtTime(800, audioCtx.currentTime + 0.1); // Zip up
+    oscillator.type = 'sawtooth'; // More "cyber" sound
+    oscillator.frequency.setValueAtTime(120, audioCtx.currentTime);
+    oscillator.frequency.linearRampToValueAtTime(800, audioCtx.currentTime + 0.05); // Faster zip
 
-    gainNode.gain.setValueAtTime(0.05, audioCtx.currentTime); // Low volume
-    gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 0.1);
+    gainNode.gain.setValueAtTime(0.04, audioCtx.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 0.05);
 
     oscillator.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 
     oscillator.start();
-    oscillator.stop(audioCtx.currentTime + 0.1);
+    oscillator.stop(audioCtx.currentTime + 0.05);
 }
 
 // Add hover listeners to interactive cards
