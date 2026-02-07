82a4e7957d0d1be7b2abe25eb6b93e867f9a13c7
Patrickleondev <gkpl0010@gmail.com>
2026-02-07 13:51:42 +0000

    feat: add contact page with form and replace email text with icons

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/portfolio/', // Repo name is 'portfolio'
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',
-                about: 'about.html',
-                expertise: 'expertise.html',
-                projects: 'projects.html',
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                expertise: resolve(__dirname, 'expertise.html'),
+                projects: resolve(__dirname, 'projects.html'),
+                contact: resolve(__dirname, 'contact.html'),
             },
         },
     },
